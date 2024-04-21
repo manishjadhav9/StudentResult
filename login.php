2396d9274719ec726e850f6863a274ebcd145a19
Manish Jadhav <manishsj289@gmail.com>
2024-04-21 14:55:52 +0530

    modified login page

--- login.php
+++ login.php
@@ -1,30 +1,3 @@
-<?php
-session_start();
-error_reporting(0);
-include('includes/config.php');
-if ($_SESSION['alogin'] != '') {
-  $_SESSION['alogin'] = '';
-}
-if (isset($_POST['login'])) {
-  $uname = $_POST['username'];
-  $password = md5($_POST['password']);
-  $sql = "SELECT UserName,Password FROM admin WHERE UserName=:uname and Password=:password";
-  $query = $dbh->prepare($sql);
-  $query->bindParam(':uname', $uname, PDO::PARAM_STR);
-  $query->bindParam(':password', $password, PDO::PARAM_STR);
-  $query->execute();
-  $results = $query->fetchAll(PDO::FETCH_OBJ);
-  if ($query->rowCount() > 0) {
-    $_SESSION['alogin'] = $_POST['username'];
-    echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
-  } else {
-
-    echo "<script>alert('Invalid Details');</script>";
-  }
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -36,126 +9,77 @@ if (isset($_POST['login'])) {
   <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
   <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
   <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
-  <link rel="stylesheet" href="css/prism/prism.css" media="screen"> 
-  <link rel="stylesheet" href="css/index.css" media="screen">
+  <link rel="stylesheet" href="css/prism/prism.css" media="screen">
+  <link rel="stylesheet" href="css/login.css" media="screen">
+  <!-- <link rel="stylesheet" href="css/index.css" media="screen"> -->
   <script src="js/modernizr/modernizr.min.js"></script>
 </head>
 
 <body class="">
   <div class="main-wrapper">
-
-    <div class="">
+    <h1 class="title text-center">Student Result Generation System</h1>
+    <div class="container">
       <div class="row">
-        <h1 align="center">Student Result Generation System</h1>
-        <div class="col-lg-6 visible-lg-block">
-
+        <div class="col-md-6">
           <section class="section">
-            <div class="row mt-40">
-              <div class="col-md-10 col-md-offset-1 pt-50">
-
-                <div class="row mt-30 ">
-                  <div class="col-md-11">
-                    <div class="panel">
-                      <div class="panel-heading">
-                        <div class="panel-title text-center">
-                          <h4>For Students</h4>
-                        </div>
-                      </div>
-                      <div class="panel-body p-20">
-
-                        <div class="panel-title text-center">
-                          <h4 class="sub-title">Student Result Generation System</h4>
-                        </div>
-
-                        <form class="form-horizontal" method="post">
-                          <div class="form-group">
-                          <div class="panel-title text-center">
-                          <p><a href="find-result.php">Search for your Results</a></p>
-                          </div>
-                            
-                            <div class="col-sm-6">
-                              
-                            </div>
-                          </div>
-
-                        </form>
-
-
-
-
-                      </div>
-                    </div>
-                    <!-- /.panel -->
-
-                  </div>
-                  <!-- /.col-md-11 -->
+            <div class="panel">
+              <div class="panel-heading">
+                <div class="panel-title text-center">
+                  <h4>For Students</h4>
                 </div>
-                <!-- /.row -->
               </div>
-              <!-- /.col-md-12 -->
+              <div class="panel-body p-20">
+                <div class="panel-title text-center">
+                  <h4 class="sub-title">Click here to view your results</h4>
+                </div>
+                <form class="form-horizontal" method="post">
+                  <div class="form-group">
+                    <div class="panel-title text-center">
+                      <p><a href="find-result.php">Search for your Results</a></p>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
-            <!-- /.row -->
           </section>
         </div>
-        <div class="col-lg-6">
+        <div class="col-md-6">
           <section class="section">
-            <div class="row mt-40">
-              <div class="col-md-10 col-md-offset-1 pt-50">
-
-                <div class="row mt-30 ">
-                  <div class="col-md-11">
-                    <div class="panel">
-                      <div class="panel-heading">
-                        <div class="panel-title text-center">
-                          <h4>Admin Login</h4>
-                        </div>
-                      </div>
-                      <div class="panel-body p-20">
-
-                        <div class="section-title">
-                          <p class="sub-title">Student Result Generation System</p>
-                        </div>
-
-                        <form class="form-horizontal" method="post">
-                          <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-2 control-label">Username</label>
-                            <div class="col-sm-10">
-                              <input type="text" name="username" class="form-control" id="inputEmail3" placeholder="UserName">
-                            </div>
-                          </div>
-                          <div class="form-group">
-                            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                            <div class="col-sm-10">
-                              <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="Password">
-                            </div>
-                          </div>
-                          <div class="form-group mt-20">
-                            <div class="col-sm-offset-2 col-sm-10">
-                              <button type="submit" name="login" value="Login" class="btn btn-success btn-labeled pull-right">Sign in<span class="btn-label btn-label-right"><i class="fa fa-check"></i></span></button>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
-                    </div>
-                    <!-- /.panel -->
-                    <p class="text-muted text-center"><small>Copyright © 2020 Student Result Generation System </a></small> </p>
-                  </div>
-                  <!-- /.col-md-11 -->
+            <div class="panel">
+              <div class="panel-heading">
+                <div class="panel-title text-center">
+                  <h4>For Admin</h4>
                 </div>
-                <!-- /.row -->
               </div>
-              <!-- /.col-md-12 -->
+              <div class="panel-body p-20">
+                <div class="section-title">
+                  <p class="sub-title text-center">Admin Login Panel</p>
+                </div>
+                <form class="form-horizontal" method="post">
+                  <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-4 control-label">Username</label>
+                    <div class="col-sm-8">
+                      <input type="text" name="username" class="form-control" id="inputEmail3" placeholder="UserName">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputPassword3" class="col-sm-4 control-label">Password</label>
+                    <div class="col-sm-8">
+                      <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    </div>
+                  </div>
+                  <div class="form-group mt-20">
+                    <div class="col-sm-offset-4 col-sm-8">
+                      <button type="submit" name="login" value="Login" class="btn btn-success btn-labeled pull-right">Sign in<span class="btn-label btn-label-right"><i class="fa fa-check"></i></span></button>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
-            <!-- /.row -->
           </section>
-
         </div>
-        <!-- /.col-md-6 -->
       </div>
-      <!-- /.row -->
     </div>
-    <!-- /. -->
-
   </div>
   <!-- /.main-wrapper -->
 
